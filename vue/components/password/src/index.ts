81b5b4de10c4961080d32e442632faaba1beeb91
Mehdi M <hi@mehdi.cc>
2022-01-19 00:07:30 +0100

    Add entry points for packages and types

---
+++ vue/components/password/src/index.ts
@@ -0,0 +1,5 @@
+import Password from "./components/Password.vue"
+export default Password;
+
+// // In Node 12+ this should work:
+// export { default as Password } from "./components/Password.vue"
---
+++ vue/components/password/types/index.d.ts
@@ -0,0 +1,3 @@
+import Password from "../src/index";
+
+export declare const Password: import("vue").DefineComponent;
