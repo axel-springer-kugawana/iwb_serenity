3e8de70e0277133f76fd0376ec4e190ce1f73461
Mehdi M <hi@mehdi.cc>
2022-01-18 23:58:18 +0100

    Add base files for component development

---
+++ vue/components/password/CHANGELOG.md
@@ -0,0 +1,31 @@
+# Changelog
+
+This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html) starting 1.0.
+
+## Unreleased
+
+Everything.
+
+**⚠️ Nothing is released at this point.** Below is only a placeholder document.
+
+<!-- Compare with [last published version](https://github.com/meduzen/datetime-attribute/compare/1.1.0...main).
+
+### New
+
+Nothing.
+
+### Changed
+
+Nothing.
+
+### Fixed
+
+Nothing.
+
+### Under the hood
+
+Nothing.
+
+## v0.1 (2022-01-xx)
+
+First published version. -->
---
+++ vue/vue-2-components/password/.eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+    root: true,
+    env: {
+        "browser": true,
+        "commonjs": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "plugin:vue/recommended",
+        "eslint:recommended",
+        "@vue/typescript/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "windows"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+        "no-unused-vars": 0,
+        "no-console": "warn",
+        "object-shorthand": ["error", "never"], /// https://eslint.org/docs/rules/object-shorthand
+        "vue/html-indent": ["error", 4],
+        "vue/script-indent": ["error", 4],
+        "vue/attributes-order": 0,
+        "vue/html-self-closing": ["error", {
+            "html": {
+                "void": "any",
+                "normal": "any",
+                "component": "any"
+            },
+            "svg": "any"
+        }],
+        "vue/html-closing-bracket-newline": 0,
+        "vue/multiline-html-element-content-newline": 0,
+        "vue/singleline-html-element-content-newline": 0,
+        "vue/html-closing-bracket-spacing": 0,
+        "no-prototype-builtins": 0,
+        "vue/return-in-computed-property": 0,
+        "vue/no-v-html": 0,
+        "vue/no-use-v-if-with-v-for": 0,
+        "@typescript-eslint/explicit-module-boundary-types": [
+            1,
+            {
+                "allowedNames": ["getCriteria"]
+            }
+        ],
+    },
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)"
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+};
